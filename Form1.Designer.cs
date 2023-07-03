7e55dc4a547a6a719814d8ed72d9292c60c85b61
Tarık Yüzgül <yuzgultarik@gmail.com>
2023-07-04 02:29:59 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace BetechBackup
+{
+    partial class BetechBackup_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_database = new System.Windows.Forms.TextBox();
+            this.textBox_path = new System.Windows.Forms.TextBox();
+            this.btn_sec = new System.Windows.Forms.Button();
+            this.btn_yedekle = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbDBName = new System.Windows.Forms.ComboBox();
+            this.cmbServerName = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 127);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seçilen Veritabanı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 172);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dosya Yolu : ";
+            // 
+            // textBox_database
+            // 
+            this.textBox_database.Location = new System.Drawing.Point(155, 123);
+            this.textBox_database.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_database.Name = "textBox_database";
+            this.textBox_database.Size = new System.Drawing.Size(336, 22);
+            this.textBox_database.TabIndex = 4;
+            this.textBox_database.Text = "Seçilen Database";
+            // 
+            // textBox_path
+            // 
+            this.textBox_path.Location = new System.Drawing.Point(155, 166);
+            this.textBox_path.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_path.Name = "textBox_path";
+            this.textBox_path.Size = new System.Drawing.Size(260, 22);
+            this.textBox_path.TabIndex = 5;
+            // 
+            // btn_sec
+            // 
+            this.btn_sec.Location = new System.Drawing.Point(424, 166);
+            this.btn_sec.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_sec.Name = "btn_sec";
+            this.btn_sec.Size = new System.Drawing.Size(68, 28);
+            this.btn_sec.TabIndex = 6;
+            this.btn_sec.Text = "...";
+            this.btn_sec.UseVisualStyleBackColor = true;
+            this.btn_sec.Click += new System.EventHandler(this.btn_sec_Click);
+            // 
+            // btn_yedekle
+            // 
+            this.btn_yedekle.Location = new System.Drawing.Point(155, 206);
+            this.btn_yedekle.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_yedekle.Name = "btn_yedekle";
+            this.btn_yedekle.Size = new System.Drawing.Size(337, 25);
+            this.btn_yedekle.TabIndex = 7;
+            this.btn_yedekle.Text = "YEDEKLE";
+            this.btn_yedekle.UseVisualStyleBackColor = true;
+            this.btn_yedekle.Click += new System.EventHandler(this.btn_yedekle_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 84);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Veri Tabanlarım :";
+            // 
+            // cmbDBName
+            // 
+            this.cmbDBName.FormattingEnabled = true;
+            this.cmbDBName.Location = new System.Drawing.Point(155, 79);
+            this.cmbDBName.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbDBName.Name = "cmbDBName";
+            this.cmbDBName.Size = new System.Drawing.Size(336, 24);
+            this.cmbDBName.TabIndex = 9;
+            this.cmbDBName.SelectedIndexChanged += new System.EventHandler(this.comboBox_veritabani_SelectedIndexChanged);
+            // 
+            // cmbServerName
+            // 
+            this.cmbServerName.FormattingEnabled = true;
+            this.cmbServerName.Location = new System.Drawing.Point(156, 39);
+            this.cmbServerName.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbServerName.Name = "cmbServerName";
+            this.cmbServerName.Size = new System.Drawing.Size(336, 24);
+            this.cmbServerName.TabIndex = 12;
+            this.cmbServerName.SelectedIndexChanged += new System.EventHandler(this.cmbServerName_SelectedIndexChanged);
+            // 
+            // BetechBackup_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 411);
+            this.Controls.Add(this.cmbServerName);
+            this.Controls.Add(this.cmbDBName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_yedekle);
+            this.Controls.Add(this.btn_sec);
+            this.Controls.Add(this.textBox_path);
+            this.Controls.Add(this.textBox_database);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "BetechBackup_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BetechBackup";
+            this.Load += new System.EventHandler(this.BetechBackup_Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_database;
+        private System.Windows.Forms.TextBox textBox_path;
+        private System.Windows.Forms.Button btn_sec;
+        private System.Windows.Forms.Button btn_yedekle;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbDBName;
+        private System.Windows.Forms.ComboBox cmbServerName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,270 @@
+﻿using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data.Sql;
+using Microsoft.SqlServer.Management.Smo.Agent;
+
+namespace BetechBackup
+{
+    public partial class BetechBackup_Form : Form
+    {
+        public BetechBackup_Form()
+        {
+            InitializeComponent();
+        }
+
+        string secilen_veritabani;
+        string path = "D:\\Backupyedek";
+
+        string[] database_array;
+
+        private void BetechBackup_Form_Load(object sender, EventArgs e)
+        {
+            /*
+
+            var server = new Microsoft.SqlServer.Management.Smo.Server(".\\YUZGUL");
+            var database_num = 0;
+
+            foreach (Database db in server.Databases)
+            {
+                comboBox_veritabani.Items.Add(db.Name);
+                listBox_veritabanlari.Items.Add(db.Name);
+                database_num++;
+            }
+            database_array = new string[database_num];
+
+            int i = 0;
+            foreach (Database db in server.Databases)
+            {
+                database_array[i] = db.Name;
+                Console.WriteLine(database_array[i]);
+                i++;
+            }
+            textBox_path.Text = path;
+            */
+
+            string ServerName = Environment.MachineName;
+            Microsoft.Win32.RegistryView registryView = Environment.Is64BitOperatingSystem ? Microsoft.Win32.RegistryView.Registry64 : Microsoft.Win32.RegistryView.Registry32;
+            using (Microsoft.Win32.RegistryKey hklm = Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, registryView))
+            {
+                Microsoft.Win32.RegistryKey instanceKey = hklm.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL", false);
+                if (instanceKey != null)
+                {
+                    foreach (var instanceName in instanceKey.GetValueNames())
+                    {
+                        if (instanceName == "MSSQLSERVER")
+                        {
+                            cmbServerName.Items.Add(ServerName);
+                        }
+                        else
+                        {
+                            cmbServerName.Items.Add(ServerName + " \\" + instanceName);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btn_sec_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Yedeklenecek yolu seçiniz.";
+            saveFileDialog1.Filter = "Yedekleme Dosyaları(*.bak) | *.bak |Tüm Dosyalar(*.*) |*.*";
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox_path.Text = fbd.SelectedPath;
+            }
+
+            /*
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox_path.Text = saveFileDialog1.FileName;
+            }
+            */
+        }
+
+        //string temp_adres = "C:\\backupyedek";
+
+        private void btn_yedekle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = "C:\\backupyedek";
+
+                bool exists = Directory.Exists(path);
+                if (exists)
+                {
+                }
+                else
+                {
+                    Directory.CreateDirectory(path);
+                }
+                SqlConnection dbcon = new SqlConnection("Data Source=" + cmbServerName.Text + ";Initial Catalog=" + cmbDBName.Text + ";" + "Integrated Security=True;");
+                string database = dbcon.Database.ToString();
+                string cmd = @"BACKUP DATABASE [" + cmbDBName.Text + "] TO DISK= '" + path + "\\" + "database-" + cmbDBName.Text +"-"+ DateTime.Now.ToString("yyyy-MM-dd--HH-mm-ss") + ".bak'";
+                dbcon.Open();
+                SqlCommand command = new SqlCommand(cmd, dbcon);
+                command.ExecuteNonQuery();
+                dbcon.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Yedek alınırken bir sorun oluştu");
+            }
+            /*            
+            for (int i = 0; i < database_array.Length; i++)
+            {
+                try
+                {
+                    SqlConnection dbcon = new SqlConnection("Data Source=.\\YUZGUL;Initial Catalog=" +   database_array[i] + ";" + "Integrated Security=True;");
+                    string database = dbcon.Database.ToString();
+
+                    string temp_adres = "C:\\backupyedek";
+                    string cmd = @"BACKUP DATABASE [" +   database_array[i] + "] TO DISK= '" + temp_adres + "\\" + "database-" +   database_array[i] + DateTime.Now.ToString("yyyy-MM-dd--HH-mm-ss") + ".bak'";
+                    dbcon.Open();
+                    SqlCommand command = new SqlCommand(cmd, dbcon);
+                    command.ExecuteNonQuery();
+                    dbcon.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Backup işleminde bir hata var : " + ex.Message);
+                }
+            }
+
+            */
+            /* Benim yaptığım 
+            SqlConnection dbcon = new SqlConnection("Data Source=.\\YUZGUL;Initial Catalog=tarikyuzgul;Integrated Security=True;");
+            try
+            {
+            dbcon.Open();
+                MessageBox.Show("Bağlantı hazır");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bağlantı hazır değil");
+            }
+            Server dbservercon = new Server(new ServerConnection());
+*/
+            /* İnternette yapılan uygulama 
+            //string connectionString = @"Data Source=YUZGUL/YUZGUL;Integrated Security=True;";
+            Server dbServer = new Server(new ServerConnection(textBox_server.Text));
+            MessageBox.Show(dbServer.Name);
+            Backup dbBackup = new Backup();
+            dbBackup.Action = BackupActionType.Database;
+            dbBackup.Database = textBox_database.Text;
+            dbBackup.Devices.AddDevice(textBox_path.Text, DeviceType.File);
+            dbBackup.Initialize = false;
+            //dbBackup.Complete += DbBackup_Complete;
+            dbBackup.SqlBackup(dbServer);
+*/
+        }
+        private void DbBackup_Complete(object sender, ServerMessageEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Yedekleme işlemi başarılı bir şekilde tamamlanmıştır.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void comboBox_veritabani_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //secilen_veritabani = cmbDBName.Text;
+            textBox_database.Text = cmbDBName.Text;
+        }
+
+        private void button_klasor_optimizasyon_Click(object sender, EventArgs e)
+        {
+            string dizin = "C:\\backupyedek";
+            var tumDosyalar = Directory.GetFiles(dizin, "*.*", SearchOption.AllDirectories);
+
+            int min_dosya_sayisi = 0;
+            var en_eski_temp = DateTime.Now;
+
+            foreach (var dosya in tumDosyalar)
+            {
+                min_dosya_sayisi++;
+                Console.WriteLine(dosya);
+                var dateTime = File.GetCreationTime(dosya) /*.ToString("yyyy-MM-dd--HH-mm-ss")*/;
+                //dateTime.AddDays(10);
+                Console.WriteLine(dateTime.Second.ToString());
+                var tarih_farki = (DateTime.Now - dateTime)/*.ToString("yyyy-MM-dd--HH-mm-ss")*/;
+                Console.WriteLine("Tarih farki " + tarih_farki.Minutes.ToString());
+
+                if (tarih_farki.Minutes >= 1)
+                {
+                    File.Delete(dosya);
+                }
+                var yil_farki = DateTime.Now.Year - dateTime.Year;
+                var ay_farki = DateTime.Now.Month - dateTime.Month;
+                var gun_farki = DateTime.Now.Day - dateTime.Day;
+                var saat_farki = DateTime.Now.Hour - dateTime.Hour;
+                var dakika_farki = DateTime.Now.Minute - dateTime.Minute;
+                var saniye_farki = DateTime.Now.Second - dateTime.Second;
+
+                Console.WriteLine(yil_farki + "yıl" + ay_farki + "ay_farki" + gun_farki + "gun_farki" + saat_farki + "saat_farki" + dakika_farki + "dakika_farki" + saniye_farki + "saniye_farki");
+
+            }
+            Console.ReadLine();
+            // 
+            Console.WriteLine(min_dosya_sayisi);
+
+            if (min_dosya_sayisi > 3)
+            {
+                var en_eski = DateTime.Now;
+            }
+
+        }
+
+        private void cmbServerName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+            Server dbServer = new Server(new ServerConnection(cmbServerName.Text));
+            //MessageBox.Show(dbServer.Name);
+            string conString = "Data Source ="+ dbServer.Name.ToString() + "; Integrated Security=True";
+            //DESKTOP - IL6RP3E\SQLEXPRESS
+            SqlConnection connection = new SqlConnection(conString);
+            connection.Open();
+            var command = new System.Data.SqlClient.SqlCommand();   
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "select name from master.sys.databases";
+
+            var adapter = new System.Data.SqlClient.SqlDataAdapter(command);
+            var dataset = new DataSet();
+            adapter.Fill(dataset);
+            DataTable dtDatabases = dataset.Tables[0];
+
+
+            cmbDBName.Items.Clear();
+            for (int i = 0; i < dataset.Tables[0].Rows.Count; i++)
+            {
+                cmbDBName.Items.Add(dataset.Tables[0].Rows[i][0].ToString());
+                connection.Close();
+            }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Geçersiz bir sunucu seçtiniz!!!");
+            }
+        }
+    }
+}
